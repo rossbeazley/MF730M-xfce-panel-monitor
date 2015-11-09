ba13d4a31f1caa8490ae8c2df13f3f2ca073aee2
rossbeazley <ross@rossbeazley.co.uk>
2015-11-09 12:46:51 +0000

    made a single line version of the command

--- generic-monitor/api.sh
+++ generic-monitor/api.sh
@@ -29,9 +29,16 @@ function ifattached {
 
 }
 
+function status_single {
+  echo $(curl --connect-timeout 1 -s "http://192.168.0.1/goform/goform_get_cmd_process?multi_data=1&isTest=false&cmd=signalbar%2Cnetwork_type" | sed 's/[{}"]//g' | sed 's/[:,]/ /g' | awk -v BASEDIR=$BASEDIR '{print "<img>" BASEDIR "/signal-" $2 ".png</img><txt> " $4 "</txt>"}')
+ 
+}
+
+#MAIN
+
 if ifattached
 then
- status
+ status_single
 else
  echo "<txt> </txt>"
 fi
